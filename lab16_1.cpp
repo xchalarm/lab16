3e8c8b5fdce2514f87bcec43b9365db78a893b13
xchalarm <tharana4671@gmail.com>
2023-02-07 15:12:39 +0700

    16_1 completed

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -1,9 +1,22 @@
-include <iostream>
+#include<iostream>
 using namespace std;
 
 int main(){
 
+	int a = 5, b = 'A', &c = b;
+	int *x = &a , *y = &b , **z = &x;
+	
+    cout << a << ' ' << char(b) << ' ' << char(c) << ' ' << x << ' ' << y << ' ' << z << '\n';
+	cout << &a << ' ' << &b << ' ' << &c << ' ' << &x << ' ' << &y << ' ' << &z <<'\n';
+	c = 'F';
+	cout << a << ' ' << char(b) << ' ' << char(c) << ' ' << x << ' ' << y << ' ' << z << '\n';
+	*y = 'W';
+	cout << a << ' ' << char(b) << ' ' << char(c) << ' ' << x << ' ' << y << ' ' << z << '\n';
+	*x = 6;
+	cout << a << ' ' << char(b) << ' ' << char(c) << ' ' << x << ' ' << y << ' ' << z << '\n';
+	**z = 7;
+	cout << a << ' ' << char(b) << ' ' << char(c) << ' ' << x << ' ' << y << ' ' << z << '\n';
 	
 	
-	return 0;
+    return 0;
 }
